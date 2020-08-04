59c35f337fc96e1c2224a11ff2caca7fb2c7854e
Eric Yu <izetbegovic21@gmail.com>
2020-08-04 00:44:06 -0400

    updated static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
